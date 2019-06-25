b845d2fbb44586a179f2ec37675d4c0549a0f2c1
kekohuke <43626569+kekohuke@users.noreply.github.com>
2019-06-25 16:14:03 -0700

    Add files via upload

---
+++ Lab2_introToAVR/source/main.c
@@ -0,0 +1,28 @@
+/*	Author: jxie031
+ *  Partner(s) Name: Xichao Wang
+ *	Lab Section:
+ *	Assignment: Lab 2  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
+				// Initialize output on PORTB to 0x00
+	unsigned char cntavail;
+	unsigned char temp_A;
+	while(1) {
+		temp_A = PINA;
+		cntavail = (temp_A & 0x01) + (temp_A & 0x02) + (temp_A & 0x04) + (temp_A & 0x80);
+		PORTB = cntavail;
+	
+	}
+	return 1;
+}
